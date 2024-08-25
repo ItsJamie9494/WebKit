e95fdb80453031a92b4a5051941e0344dd929a2f
Jamie Murphy <hello@itsjamie.dev>
2024-08-25 14:36:10 -0700

    [GTK] Add IconGtk
    This is a small implementation of WebCore::Icon that holds a GIcon. It's
    required for WebExtensions. There's no implementation of any paint
    functions as they aren't needed yet, it's purely intended to hold the
    GIcon on LInux and allow WebExtensions to return one class on all
    platforms (since WebCore::Icon already has an implementation for
    CocoaImage)

--- Source/WebCore/platform/graphics/Icon.h
+++ Source/WebCore/platform/graphics/Icon.h
@@ -41,6 +41,11 @@ using CocoaImage = UIImage;
 
 #elif PLATFORM(WIN)
 typedef struct HICON__* HICON;
+
+#elif PLATFORM(GTK)
+#include <wtf/glib/GRefPtr.h>
+
+typedef struct _GIcon GIcon;
 #endif
 
 namespace WebCore {
@@ -59,6 +64,12 @@ public:
 
 #if PLATFORM(WIN)
     static Ref<Icon> create(HICON hIcon) { return adoptRef(*new Icon(hIcon)); }
+#endif
+
+#if PLATFORM(GTK)
+    WEBCORE_EXPORT static RefPtr<Icon> create(GIcon *icon);
+
+    GRefPtr<GIcon> icon() const { return m_icon; };
 #endif
 
 #if PLATFORM(COCOA)
@@ -80,6 +91,9 @@ private:
 #elif PLATFORM(WIN)
     Icon(HICON);
     HICON m_hIcon;
+#elif PLATFORM(GTK)
+    Icon(GIcon *);
+    GRefPtr<GIcon> m_icon;
 #endif
 };
 
